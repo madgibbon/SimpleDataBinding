cc79a91613e3ef48eb128014cff689633ed0d3e6
Richard Hill <richardhill101@gmail.com>
2017-11-13 19:47:12 +0000

    First commit

---
+++ SimpleDataBinding/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SimpleDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Person charlie = Init();
+            // BindingContext binds the object to assocciated XMAL view bindings
+            BindingContext = charlie;
+            InitializeComponent();
+        }
+        private Person Init()
+        {
+            return new Person { 
+                FullName = "George Washington", 
+                Phone = "1234-555-8767", 
+                Email = "george.washington@gmail.com"
+            };
+        }
+    }
+}
---
+++ SimpleDataBinding/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SimpleDataBinding
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
